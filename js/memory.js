6e7a8b22c08cc3aab42e421c1e636d27d6980ab9
Stan <15729557+stanvv1998@users.noreply.github.com>
2023-03-15 14:43:37 +0100

    first commit

---
+++ js/memory.js
@@ -0,0 +1,35 @@
+"use strict";
+
+class Card {
+	constructor(card1, card2=card1, set=card1, sound=card1){
+		this.card1 = card1;
+		this.card2 = card2;
+		this.set = set;
+		this.sound = sound;
+	}
+}
+
+const myField = document.getElementById("field");
+myField.addEventListener("click", onClickCard);
+const myCardArray = ["duck", "kitten", "piglet", "puppy", "calf", "veal", "lamb", "rooster", "horse", "mouse", "dog", "cat", "goose", "goat", "sheep", "pig", "cow", "chick", "hen"];
+
+const myCardSet = myCardArray.map(card => new Card(card));
+document.onload = populateField();
+
+function populateField() {
+	myField.innerHTML = "";
+	myCardSet.forEach(card => {
+		let newTile = document.createElement("div");
+		let newCard = document.createElement("img");
+		newTile.setAttribute("class", "board6");
+		let imageURL = "img/" + card.card1 + ".jpg";
+		newCard.setAttribute("src", imageURL);
+		newCard.setAttribute("name", card);
+		newTile.appendChild(newCard);
+		myField.appendChild(newTile);
+	});
+}
+
+function onClickCard(e) {
+	console.log(e.target.getAttribute("name"));
+}
